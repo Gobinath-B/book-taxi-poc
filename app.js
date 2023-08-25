92dbc97a39f83eda1633fcdd27e51e83e172b6bd
Gobinath <princegopinath42@gmail.com>
2023-08-25 19:52:17 +0530

    fix: res.render("bill")

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const db = require("./config");
+const { render } = require('ejs');
 const fb = db.firestore();
 const PORT = process.env.PORT || 8000;
 
@@ -59,12 +60,13 @@ app.post("/verify-otp", async(req,res)=>{
 app.post("/startTrip", async(req,res)=>{
     try{
         const {image, odometerReading} = req.body;
+        const bookingId = req.body.id;
+        //console.log({image, odometerReading});
+       await fb.collection('customer').doc(bookingId).update({start:odometerReading},)
         
-        // console.log({image, odometerReading});
-       await fb.collection('customer').doc(docId)
-        .update(odometerReading)
-        .then(docRef => {
-            console.log('Document written with ID: ', docRef.id);
+        .then(() => {
+            console.log('Document written with ID: ', bookingId);
+            res.redirect("/endForm");
         })
     }
     catch{
@@ -75,28 +77,40 @@ app.post("/startTrip", async(req,res)=>{
 app.post("/endTrip",async (req,res)=>{
     try{
          const {image, odometerReadingEnd} = req.body;
-         const customerRef = fb.collection('customer').doc(docId);
-         const customerDoc = await customerRef.get();
+         const bookingId = req.body.id;
+           
+         await fb.collection('customer').doc(bookingId).update({end:odometerReadingEnd})
+         var data =  await fb.collection('customer').doc(bookingId).get()
+         const start = data._fieldsProto.start.stringValue;
+         const end = data._fieldsProto.end.stringValue;
+         const phone = data._fieldsProto.Phone.integerValue;
+         const ratePerKm = data._fieldsProto.ratePerKm.integerValue;
+         const car = data._fieldsProto.car.stringValue;
+         const totalDistance = end - start;
+         const totalAmount = totalDistance * ratePerKm;
+         const jsonData = {
+            car: car,
+            start: start,
+            end: end,
+            ratePerKm: ratePerKm,
+            totalDistance: totalDistance,
+            totalAmount: totalAmount
+          };
+         await fb.collection('customer').doc(bookingId).update({amount:totalAmount})
+         res.render("bill",{jsonData})
         
-         
-         if (!customerDoc.exists) {
-             res.json({ error: 'Customer not found' });
-             return;
-         }
- 
-         const customerData = customerDoc.data();
-         res.render('bill',{odometerReadingEnd},{customerData});
      } catch (error) {
          console.error('Error fetching customer data:', error);
          
      }
-         
-    
-  
 })
 
 app.get("/startForm",(req,res)=>{
     res.render("startForm");
+})
+
+app.get("/bill",(req,res)=>{
+    res.render("bill");
 })
 
 app.get("/endForm",(req,res)=>{
