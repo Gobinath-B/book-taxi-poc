74cca36897c7367630e4e02c958d1de7b9819b6d
Gobinath B <princegopinath42@gmail.com>
2023-09-23 19:05:59 +0530

    chore: config storage bucket

--- config.js
+++ config.js
@@ -2,8 +2,11 @@ var admin = require("firebase-admin");
 
 var serviceAccount = require("./taxi-poc.json");
 
+// Initialize Cloud Storage and get a reference to the service
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "taxi-go-cabs-ramesh.appspot.com"
 });
 
-module.exports = admin
+module.exports = admin;
