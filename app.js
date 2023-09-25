b541de1d02804de809139579352b489da4f72298
Gobinath B <princegopinath42@gmail.com>
2023-09-25 18:18:57 +0530

    refactor: booking updation

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(express.static('Public'));
 app.use(express.urlencoded({extends:false}))
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"./views"));
+let started = false;
 
 app.post("/customer", async(req,res)=>{
     try {
@@ -73,6 +74,7 @@ app.post("/startTrip", async(req,res)=>{
         const {imageRef, odometerReading,otp} = req.body;
         const bookingId = req.body.id;
         const date = new Date();
+        started = true;
         // const time = req.body.time;
         console.log(bookingId);
         console.log(req.body);
@@ -82,7 +84,7 @@ app.post("/startTrip", async(req,res)=>{
        //console.log(data.data() , otp);
         if(data.data().otp == otp){
          //console.log(true);
-        await fb.collection('BookingDetails').doc(bookingId).update({status:"start",startOdometerDate:date,startOdometerValue:odometerReading,startBase64:imageRef})
+        await fb.collection('BookingDetails').doc(bookingId).update({status:"start",startOdometerDate:date,startOdometerValue:odometerReading,startBase64:imageRef,isAssigned:true})
         //await fb.collection('customer').doc(bookingId).update({start:odometerReading,image:imageRef})
        
           .then(async() => {
@@ -122,110 +124,141 @@ app.post("/startTrip", async(req,res)=>{
 
 app.post("/endTrip",async (req,res)=>{
     try{
-       
-         const {imageRef, odometerReadingEnd} = req.body;
-         //console.log(req.body);
-         const bookingId = req.body.id;
-         const date = new Date();
-         const additionalCharges = req.body.adicharge;
-         //console.log(date);
         
-         await fb.collection('BookingDetails').doc(bookingId).update({additionalCharge:additionalCharges,endOdometerDate:date,endOdometerValue:odometerReadingEnd,endBase64:imageRef,status:"end"})
-         var data =  await fb.collection('BookingDetails').doc(bookingId).get()
-         const name = data.data().custName;
-         const service = data.data().trip;
-         const additionalCharge = data.data().additionalCharge;
-         const driverName = data.data().driverName;
-         const beta = data.data().driverBata;
-         const from = data.data().from;
-         const to = data.data().to;
-         const start = data.data().startOdometerValue;
-         const end = data.data().endOdometerValue;
-         const phone = data.data().custPhone;
-         const ratePerKm = data.data().kmPrice;
-         const taxPercent = data.data().taxPercent;
-         const carName = data.data().carName;
-         const status = data.data().status;
-         const totalDistance = end - start;
-         let betaAmount = beta;
-         const sDate = data.data().startOdometerDate;         
-         const eDate =  data.data().endOdometerDate;
-         const StrDate = sDate.toDate().toDateString();
-         const EnDate = eDate.toDate().toDateString();
-         const startDate = new Date(StrDate);
-         const endDate = new Date(EnDate);
-         const totalTime = startDate.getTime() - endDate.getTime();
-         let totalDays = Math.abs(Math.ceil(totalTime / (1000 * 3600 * 24))); 
-         totalDays +=1;
-         let totalAmount ;
-         let amount ;
-         console.log("date:"+startDate);
-         console.log("date:"+endDate);
-         console.log(ratePerKm);
-         console.log(totalDistance);
-         console.log(totalAmount);
-         console.log(beta);
-         console.log(betaAmount);
-         console.log(amount);
-        
-         if(service == "One Way Trip"){
-            if(totalDistance <130){
-                amount = 130 * parseInt(ratePerKm);
+            const {imageRef, odometerReadingEnd} = req.body;
+            //console.log(req.body);
+            const bookingId = req.body.id;
+            const date = new Date();
+            const additionalCharges = req.body.adicharge;
+            //console.log(date);
+           
+            await fb.collection('BookingDetails').doc(bookingId).update({additionalCharge:additionalCharges,endOdometerDate:date,endOdometerValue:odometerReadingEnd,endBase64:imageRef,status:"Trip Completed",isCompleted: true})
+            var data =  await fb.collection('BookingDetails').doc(bookingId).get()
+            const name = data.data().custName;
+            const service = data.data().trip;
+            const additionalCharge = data.data().additionalCharge;
+            const driverName = data.data().driverName;
+            const beta = data.data().driverBata;
+            const from = data.data().from;
+            const to = data.data().to;
+            const start = data.data().startOdometerValue;
+            const end = data.data().endOdometerValue;
+            const phone = data.data().custPhone;
+            const ratePerKm = data.data().kmPrice;
+            const taxPercent = data.data().taxPercent;
+            const carName = data.data().carName;
+            const status = data.data().status;
+            const totalDistance = end - start;
+            let betaAmount = beta;
+            const sDate = data.data().startOdometerDate;         
+            const eDate =  data.data().endOdometerDate;
+            const StrDate = sDate.toDate().toDateString();
+            const EnDate = eDate.toDate().toDateString();
+            const startDate = new Date(StrDate);
+            const endDate = new Date(EnDate);
+            const totalTime = startDate.getTime() - endDate.getTime();
+            let totalDays = Math.abs(Math.ceil(totalTime / (1000 * 3600 * 24))); 
+            totalDays +=1;
+            let totalAmount ;
+            let amount ;
+            console.log("date:"+startDate);
+            console.log("date:"+endDate);
+            console.log(ratePerKm);
+            console.log(totalDistance);
+            console.log(totalAmount);
+            console.log(beta);
+            console.log(betaAmount);
+            console.log(amount);
+           
+            if(service == "One Way Trip"){
+               if(totalDistance <130){
+                   amount = 130 * parseInt(ratePerKm);
+               }
+               else{
+                  amount = parseInt(totalDistance) * parseInt(ratePerKm);
+               }
             }
             else{
-               amount = parseInt(totalDistance) * parseInt(ratePerKm);
+               if(totalDistance <= 250 * totalDays){
+                   let distance = (totalDays*250);
+                   amount = distance * parseInt(ratePerKm);
+               }
+               else{
+                   amount = parseInt(totalDistance) * parseInt(ratePerKm);   
+               }
             }
-         }
-         else{
-            if(totalDistance <= 250 * totalDays){
-                let distance = (totalDays*250);
-                amount = distance * parseInt(ratePerKm);
-            }
-            else{
-                amount = parseInt(totalDistance) * parseInt(ratePerKm);   
-            }
-         }
-         
-         if(parseInt(totalDays) == 1){
-            totalAmount =  parseInt(amount) + parseInt(beta)+ parseInt(additionalCharge);
-         }
-         else{
             
-            betaAmount = parseInt(totalDays) * beta ; 
-            totalAmount = parseInt(amount) + parseInt(betaAmount)+ parseInt(additionalCharge);
-         }
-         const jsonData = {
-            bookingId:bookingId,
-            service:service,
-            driverName: driverName,
-            name:name,
-            from:from,
-            to:to,
-            phone:phone,
-            carName: carName,
-            start: start,
-            end: end,
-            ratePerKm: ratePerKm,
-            betaAmount: betaAmount,
-            totalDistance: totalDistance,
-            totalAmount: totalAmount
-          };
-          await fb.collection('BookingDetails').doc(bookingId).update({totalTravelingTime:totalDays});
-          //console.log(totalAmount);
-         //await fb.collection('BookingDetails').doc(bookingId).update({amount:totalAmount,travel_distance:totalDistance})
-         //.then(()=>{
-            res.render("bill",{jsonData});
-         //})
-         
+            if(parseInt(totalDays) == 1){
+               totalAmount =  parseInt(amount) + parseInt(beta)+ parseInt(additionalCharge);
+            }
+            else{
+               
+               betaAmount = parseInt(totalDays) * beta ; 
+               totalAmount = parseInt(amount) + parseInt(betaAmount)+ parseInt(additionalCharge);
+            }
+            const jsonData = {
+               bookingId:bookingId,
+               service:service,
+               driverName: driverName,
+               name:name,
+               from:from,
+               to:to,
+               phone:phone,
+               carName: carName,
+               start: start,
+               end: end,
+               ratePerKm: ratePerKm,
+               betaAmount: betaAmount,
+               totalDistance: totalDistance,
+               totalAmount: totalAmount
+             };
+             await fb.collection('BookingDetails').doc(bookingId).update({totalTravelingTime:totalDays+"day(s)",days:totalDays,driverBata:betaAmount,totalAmount:totalAmount});
+             //console.log(totalAmount);
+            //await fb.collection('BookingDetails').doc(bookingId).update({amount:totalAmount,travel_distance:totalDistance})
+            //.then(()=>{
+               res.render("bill",{jsonData});
+            //})
         
-     } catch (error) {
+        
+         
+}    
+      catch (error) {
          console.error('Error fetching customer data:', error);
          
      }
-})
+    });
 
-app.get("/startForm",(req,res)=>{
+app.get("/startForm",async(req,res)=>{
+
+    if(started == false){
     res.render("startForm");
+    }
+    else{
+        const bookingId = req.body.bookingId;
+        console.log(bookingId);
+        var data =  await fb.collection('BookingDetails').doc(bookingId).get();
+                const customerData = data.data();
+                const from = customerData.from;
+                const to = customerData.to;
+                const start = customerData.startOdometerValue;
+                const estimatedDistance = customerData.estimatedDistance;
+                const ratePerKm = customerData.kmPrice;
+                const estimatedTime = customerData.totalTravelingTime;
+                const estimatedAmount = customerData.totalAmountCalculated;
+                
+                const jsonData = {
+                   from:from,
+                   to:to,
+                   start:start,
+                   estimatedDistance:estimatedDistance,
+                   estimatedTime : estimatedTime,
+                   ratePerKm:ratePerKm,
+                   estimatedAmount :estimatedAmount
+                }
+               
+               
+        res.render("estimation.ejs",{jsonData,customerData:customerData});
+       }
 })
 
 // app.get("/estimation",async(req,res)=>{
@@ -262,7 +295,7 @@ app.get("/booking/:id",async(req,res)=>{
         const customerDoc = await customerRef.get();
         console.log(customerDoc);
         if (!customerDoc.exists) {
-            res.json({ error: 'Customer not found',docId:docId });
+            res.json({ error: 'Booking not found',docId:docId });
             // console.log(req.params.id);
             return;
         }
@@ -280,8 +313,37 @@ app.get("/bill",(req,res)=>{
     res.render("bill");
 })
 
-app.get("/endForm",(req,res)=>{
+app.get("/endForm",async(req,res)=>{
+  
+   if(started == true){
     res.render("endForm");
+   }
+   else{
+    const bookingId = req.body.bookingId;
+    console.log(bookingId);
+    var data =  await fb.collection('BookingDetails').doc(bookingId).get();
+            const customerData = data.data();
+            const from = customerData.from;
+            const to = customerData.to;
+            const start = customerData.startOdometerValue;
+            const estimatedDistance = customerData.estimatedDistance;
+            const ratePerKm = customerData.kmPrice;
+            const estimatedTime = customerData.totalTravelingTime;
+            const estimatedAmount = customerData.totalAmountCalculated;
+            
+            const jsonData = {
+               from:from,
+               to:to,
+               start:start,
+               estimatedDistance:estimatedDistance,
+               estimatedTime : estimatedTime,
+               ratePerKm:ratePerKm,
+               estimatedAmount :estimatedAmount
+            }
+           
+           
+    res.render("estimation.ejs",{jsonData,customerData:customerData});
+   }
 });
 
 // app.post("/location",(req,res)=>{
